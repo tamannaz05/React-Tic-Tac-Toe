217237e0c4d9a5a54b12e77b9a4428542144cd47
tamannaz05 <tamannaparakh5@gmail.com>
2024-12-13 00:08:02 +0530

    first time

---
+++ src/components/Players.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Players({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(isEditing ? false : true);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editPlayerName = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "EDIT";
+  if (isEditing) {
+    editPlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    buttonCaption = "SAVE";
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editPlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
+    </li>
+  );
+}
---
+++ src/components/example.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const example = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default example;
